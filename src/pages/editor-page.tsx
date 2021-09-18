877ccb85d8e103a31de9145d81d7aa62d0f8ddbb
CallumHoward <callum.howard@safetyculture.io>
2021-09-18 22:34:49 +1000

    Started on top nav

--- src/pages/editor-page.tsx
+++ src/pages/editor-page.tsx
@@ -1,6 +1,31 @@
 import React, { FunctionComponent } from "react";
+import styled from "styled-components";
 import { QuestionsList } from "../components/questions-list";
 
+const PageContainer = styled.div``;
+
+const NavBar = styled.header`
+  width: 100%;
+  min-height: 6vh;
+  padding: 0 1rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: #ffffff;
+`;
+
 export const EditorPage: FunctionComponent = () => {
-  return <QuestionsList />;
+  return (
+    <PageContainer>
+      <NavBar>
+        <button>Back</button>
+      </NavBar>
+      <QuestionsList />
+    </PageContainer>
+  );
 };
