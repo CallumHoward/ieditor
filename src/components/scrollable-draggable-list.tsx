657c4f5a57bca615dfc8bb46e5b72d5c4bdc71b1
Jane Fan <jane.fan@airtasker.com>
2021-09-19 13:49:01 +1000

    simplify re-render logic

--- src/components/scrollable-draggable-list.tsx
+++ src/components/scrollable-draggable-list.tsx
@@ -74,7 +74,7 @@ const SnapScrollContainer = styled.div`
   scroll-snap-align: start;
 `;
 
-const ScrollableDraggableListBase: FunctionComponent<ScrollableDraggableListProps> =
+const ScrollableDraggableList: FunctionComponent<ScrollableDraggableListProps> =
   ({ initialItems, height, currentIndex, onChangeIndex }) => {
     const [items, setItems] = useState<ListItem[]>(initialItems);
     const listContainerRef = useRef<HTMLDivElement | null>(null);
@@ -199,17 +199,5 @@ const ScrollableDraggableListBase: FunctionComponent<ScrollableDraggableListProp
       </ComponentContainer>
     );
   };
-
-const ScrollableDraggableList = React.memo(
-  ScrollableDraggableListBase,
-  (prevProps, nextProps) =>
-    prevProps.currentIndex.value === nextProps.currentIndex.value &&
-    prevProps.currentIndex.shouldAutoScroll ===
-      nextProps.currentIndex.shouldAutoScroll &&
-    prevProps.initialItems === nextProps.initialItems &&
-    prevProps.height === nextProps.height &&
-    prevProps.onChangeIndex === nextProps.onChangeIndex &&
-    prevProps.children === nextProps.children
-);
 
 export { ScrollableDraggableList };
